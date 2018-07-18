91da49a0ae2ff1b9fc3e11e4d19d69d20105b546
vinogor <andreev.aleksandr.spb@gmail.com>
2018-07-18 15:07:11 +0300

    4 файла с ДЗ, есть вопросы, написал их в комментах

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,29 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+
+int a = 4, b = 10; // первое и второе вводимое число
+int s = 1; // результат вычислений, s = 1 - чтобы работало возведение в степень
+String mathOp = "^";
+
+	if (mathOp.equals("+"))
+		s = a + b;
+	else if (mathOp.equals("-"))
+		s = a - b;
+	else if (mathOp.equals("*"))
+		s = a * b;
+	else if (mathOp.equals("/"))
+		s = a / b;
+	else if (mathOp.equals("%"))
+		s = a % b;
+	else if (mathOp.equals("^")) {
+		for (int i = 0; i < b; i++)
+			s = s * a;
+	}
+
+	System.out.println(a + " " + mathOp + " " + b + " = " + s);
+   
+	}
+}
+
