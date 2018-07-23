6dee7543eb9669a702d5ccdd9d066900224f2d6d
vinogor <andreev.aleksandr.spb@gmail.com>
2018-07-23 17:30:02 +0300

    правки после проверки куратора chimax

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,29 +1,30 @@
-
-
 public class Calculator {
     public static void main(String[] args) {
-
-int a = 4, b = 10; // первое и второе вводимое число
-int s = 1; // результат вычислений, s = 1 - чтобы работало возведение в степень 
-String mathOp = "^";
-
-	if (mathOp.equals("+"))
-		s = a + b;
-	else if (mathOp.equals("-"))
-		s = a - b;
-	else if (mathOp.equals("*"))
-		s = a * b;
-	else if (mathOp.equals("/"))
-		s = a / b;
-	else if (mathOp.equals("%"))
-		s = a % b;
-	else if (mathOp.equals("^")) {
-		for (int i = 0; i < b; i++)
-			s = s * a;
-	}
-
-	System.out.println(a + " " + mathOp + " " + b + " = " + s);
-   
-	}
+		int firstInputNumber = 4;
+		int secondInputNumber = 10;
+		int resultOfСalculation = 1; // = 1 - чтобы работало возведение в степень 
+		String mathOperation = "^";
+		if (mathOperation.equals("+")) {
+			resultOfСalculation = firstInputNumber + secondInputNumber;
+		}
+		else if (mathOperation.equals("-")) {
+			resultOfСalculation = firstInputNumber - secondInputNumber;
+		}
+		else if (mathOperation.equals("*")) {
+			resultOfСalculation = firstInputNumber * secondInputNumber;
+		}
+		else if (mathOperation.equals("/")) {
+			resultOfСalculation = firstInputNumber / secondInputNumber;
+		}
+		else if (mathOperation.equals("%")) {
+			resultOfСalculation = firstInputNumber % secondInputNumber;
+		}
+		else if (mathOperation.equals("^")) {
+			for (int i = 0; i < secondInputNumber; i++) {
+				resultOfСalculation *= firstInputNumber;
+			}
+		}
+		System.out.println(firstInputNumber + " " + mathOperation + " " + secondInputNumber + " = " + resultOfСalculation);
+ 	}
 }
 
