da846e732a5449cbd19e387741f042cbd011dc62
vinogor <andreev.aleksandr.spb@gmail.com>
2018-08-14 18:27:12 +0300

    psql files v2

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -3,15 +3,15 @@ CREATE DATABASE jaegers;
 \l
 \connect jaegers;
 CREATE TABLE CharacteristicsOfrobots (
-	id 				SERIAL PRIMARY KEY,
-	modelName 		TEXT,
-	mark 			CHAR(6),
-	height 			INTEGER,
-	weight	 		INTEGER,
-	status 			CHAR(9),
-	origin 			TEXT,
-	launch 			DATE,
-	kaijuKill 		SMALLINT
+  id        SERIAL PRIMARY KEY,
+  modelName TEXT,
+  mark      CHAR(6),
+  height    INTEGER,
+  weight    INTEGER,
+  status    CHAR(9),
+  origin    TEXT,
+  launch    DATE,
+  kaijuKill SMALLINT
 );
 \d
 \d 'CharacteristicsOfrobots'
--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,14 +1,12 @@
-INSERT INTO Characteristicsofrobots(modelName, mark, height, weight, status, origin, launch, kaijuKill)
-	VALUES 
-		('Guardian Bravo', 'Mark-6', '240', '1975', 'Destroyed', 'USA', '2033-12-15', '2'),
-		('Titan Redeemer', 'Mark-6', '250', '2014', 'Destroyed', 'Russia', '2035-02-03', '3'),
-		('Mammoth Apostle', 'Mark-4', '184', '1751', 'Destroyed', 'Canada', '2018-10-11', '0'),
-		('Echo Saber', 'Mark-4', '174', '1813', 'Destroyed', 'Japan', '2018-01-22', '1'),
-		('Striker Eureka', 'Mark-5', '250', '1850', 'Destroyed', 'Australia', '2019-11-02', '11'),
-		('Chrome Brutus', 'Mark-3', '155', '2014', 'In use', 'Canada', '2017-05-15', '5'),
-		('Vulcan Specter', 'Mark-3', '228', '1875', 'Destroyed', 'Australia', '2017-02-01', '3'),
-		('Gipsy Danger', 'Mark-3', '260', '1980', 'Destroyed', 'USA', '2017-07-10', '9'),
-		('Eden Assassin', 'Mark-2', '178', '1547', 'Destroyed', 'Russia', '2016-10-15', '8'),
-		('Tacit Ronin', 'Mark-1', '244', '7450', 'Destroyed', 'Japan', '2015-12-06', '3')
-	;
+INSERT INTO Characteristicsofrobots (modelName, mark, height, weight, status, origin, launch, kaijuKill)
+VALUES ('Guardian Bravo', 'Mark-6', '240', '1975', 'Destroyed', 'USA', '2033-12-15', '2'),
+       ('Titan Redeemer', 'Mark-6', '250', '2014', 'Destroyed', 'Russia', '2035-02-03', '3'),
+       ('Mammoth Apostle', 'Mark-4', '184', '1751', 'Destroyed', 'Canada', '2018-10-11', '0'),
+       ('Echo Saber', 'Mark-4', '174', '1813', 'Destroyed', 'Japan', '2018-01-22', '1'),
+       ('Striker Eureka', 'Mark-5', '250', '1850', 'Destroyed', 'Australia', '2019-11-02', '11'),
+       ('Chrome Brutus', 'Mark-3', '155', '2014', 'In use', 'Canada', '2017-05-15', '5'),
+       ('Vulcan Specter', 'Mark-3', '228', '1875', 'Destroyed', 'Australia', '2017-02-01', '3'),
+       ('Gipsy Danger', 'Mark-3', '260', '1980', 'Destroyed', 'USA', '2017-07-10', '9'),
+       ('Eden Assassin', 'Mark-2', '178', '1547', 'Destroyed', 'Russia', '2016-10-15', '8'),
+       ('Tacit Ronin', 'Mark-1', '244', '7450', 'Destroyed', 'Japan', '2015-12-06', '3');
 \q
